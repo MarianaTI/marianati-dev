ac7801cba8c8c9ba62b1a723a077afedd39073db
Mariana Trujillo Islas <105942792+MarianaTI@users.noreply.github.com>
2024-05-08 08:48:31 -0600

    feat(): tools slider

--- src/components/ToolSlider/index.js
+++ src/components/ToolSlider/index.js
@@ -1,57 +1,22 @@
 import React from "react";
-import { Container, Content } from "./index.style";
+import { Container, Content, Slide } from "./index.style";
 import Image from "next/image";
-
-const tools = [
-  {
-    image: "/img/js.png",
-    name: "JavaScript",
-  },
-  {
-    image: "/img/react.png",
-    name: "React.js",
-  },
-  {
-    image: "/img/next-js.png",
-    name: "Next.js",
-  },
-  {
-    image: "/img/html5.png",
-    name: "CSS",
-  },
-  {
-    image: "/img/react.png",
-    name: "HTML",
-  },
-  {
-    image: "/img/firebase.png",
-    name: "Firebase",
-  },
-  {
-    image: "/img/jirasoftware.png",
-    name: "JiraSoftware",
-  },
-  {
-    image: "/img/figma.png",
-    name: "Figma",
-  },
-  {
-    image: "/img/adobexd.png",
-    name: "AdobeXD",
-  },
-  {
-    image: "/img/nodejs.png",
-    name: "Node.js",
-  },
-];
+import { tools } from "../../../constants";
 
 const ToolSlider = () => {
   return (
     <Container>
       {tools.map((tool, index) => (
         <Content key={index}>
-          <Image src={tool.image} height={32} width={28} alt="tools icon" />
-          <span>{tool.name}</span>
+          <Slide>
+            <Image
+              src={tool.image}
+              height={tool.height}
+              width={tool.width}
+              alt="tools icon"
+            />
+            <span>{tool.name}</span>
+          </Slide>
         </Content>
       ))}
     </Container>
--- src/components/ToolSlider/index.style.js
+++ src/components/ToolSlider/index.style.js
@@ -3,20 +3,36 @@ import styled from "@emotion/styled";
 export const Container = styled.div`
   height: 96px;
   width: 100vw;
-  background-color: #C82F8D;
+  background-color: #c82f8d;
   display: flex;
   align-items: center;
+  overflow: hidden;
 `;
 
 export const Content = styled.div`
-    display: flex;
-    gap: 16px;
-    justify-content: center;
-    align-items: center;
-    padding: 32px;
-    & span{
-        font-size: 16px;
-        color: #fff;
-        font-weight: 500;
+  padding: 32px;
+  animation: scroll 40s linear infinite;
+  animation-timing-function: ease-in-out;
+  width: calc(150px * 20);
+  & span {
+    font-size: 16px;
+    color: #fff;
+    font-weight: 500;
+  }
+  @keyframes scroll {
+    0% {
+      transform: translateX(0);
     }
+    100% {
+      transform: translateX(calc(-150px * 10));
+    }
+  }
+`;
+
+export const Slide = styled.div`
+  display: flex;
+  gap: 16px;
+  justify-content: center;
+  align-items: center;
+  width: 150px;
 `;
